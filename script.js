1dafddf5515aa81153f5beb8685ad14d2997020c
lythronax <k.samir@esisa.ac.ma>
2026-05-13 16:25:47 +0100

    Ajout d'une interaction au clic

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("JavaScript fonctionne !");
+const btnMessage = document.getElementById("btnMessage");
+const message = document.getElementById("btnMessage");
+
+btnMessage.addEventListener("click", function () {
+  message.textContent = "Bravo, tu as cliqué sur le bouton !";
+});
