e57c7eb0f2ba63cb54ad346507e6ceff9800322c
lythronax <k.samir@esisa.ac.ma>
2026-05-13 16:32:38 +0100

    Ajout d'un input interactif

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const message = document.getElementById("btnMessage");
 btnMessage.addEventListener("click", function () {
   message.textContent = "Bravo, tu as cliqué sur le bouton !";
 });
+
+const inputNom = document.getElementById("nom");
+const btnNom = document.getElementById("btnNom");
+const resultatNom = document.getElementById("resultatNom");
+
+btnNom.addEventListener("click", function () {
+  resultatNom.textContent = "Bonjour " + inputNom.value;
+});
